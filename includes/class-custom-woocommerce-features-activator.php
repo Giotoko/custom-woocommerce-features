8caf267683d602b298ef101fcdbdd1f8bebd728d
Jhonaiquel Rodríguez <46872129+Giotoko@users.noreply.github.com>
2024-10-16 20:17:19 -0400

    initial commit
    initial

---
+++ includes/class-custom-woocommerce-features-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://Jhonaiquel.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Woocommerce_Features
+ * @subpackage Custom_Woocommerce_Features/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Woocommerce_Features
+ * @subpackage Custom_Woocommerce_Features/includes
+ * @author     Jhonaiquel Rodríguez <jhonaiquel@gmail.com>
+ */
+class Custom_Woocommerce_Features_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-custom-woocommerce-features-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://Jhonaiquel.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Woocommerce_Features
+ * @subpackage Custom_Woocommerce_Features/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Woocommerce_Features
+ * @subpackage Custom_Woocommerce_Features/includes
+ * @author     Jhonaiquel Rodríguez <jhonaiquel@gmail.com>
+ */
+class Custom_Woocommerce_Features_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
